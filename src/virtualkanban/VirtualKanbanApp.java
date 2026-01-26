1bd1b4b19fe101dae05721902d525d16cb816803
Skadooshky <shivarndavidson1@gmail.com>
2026-01-26 20:40:42 +1300

    UI Updated

--- src/virtualkanban/VirtualKanbanApp.java
+++ src/virtualkanban/VirtualKanbanApp.java
@@ -4,7 +4,7 @@ import ui.MainFrame;
 
 import javax.swing.SwingUtilities;
 
-public class VirtualKanbanApp {
+public class VirtualKanbanApp{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
     }
